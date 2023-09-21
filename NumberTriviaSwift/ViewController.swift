821b65cb74322ae2c04cc6351cc00ce94bd991fe
rotem <nevgauker@gmail.com>
2023-09-21 16:02:19 +0300

    Initial Commit

---
+++ NumberTriviaSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NumberTriviaSwift
+//
+//  Created by Rotem Nevgauker on 21/09/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
